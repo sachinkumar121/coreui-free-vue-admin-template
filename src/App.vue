37c9daae4ee6f5d2ef5e9fa01532f8388bb6a1e2
sachinkumar121 <skarora7894@gmail.com>
2023-04-12 10:12:47 +0530

    add capacitorjs support.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <router-view />
 </template>
+<script>
+import { SafeArea } from 'capacitor-plugin-safe-area';
 
+export default {
+  mounted() {
+    SafeArea.getSafeAreaInsets().then(({ insets }) => {
+      // window.alert(JSON.stringify(insets));
+    });
+
+    SafeArea.getStatusBarHeight().then(({statusBarHeight}) => {
+      // window.alert('statusBarHeight');
+      // window.alert(JSON.stringify(statusBarHeight));
+    })
+  }
+}
+</script>
 <style lang="scss">
 // Import Main styles for this application
 @import 'styles/style';
